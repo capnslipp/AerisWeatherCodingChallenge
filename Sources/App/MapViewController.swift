d34681a07baa26ae3797cf05a3e684346a436dec
Slipp Douglas Thompson <git@capnslipp.com>
2023-03-18 08:54:20 -0500

    Gave MapVC a `LayerState` enum, var, and helpers; set up `layerToggleButtonPressed` action
    In `MapViewController`:
    
    * Created `LayerState` enum with each of the 4 map layer states (`radar`, `alerts`, `stormReportsPoionts`, & `stormReportsHeatmap`) and a `layerState` ivar.
    * Created `allLayerToggleButtons`, `layerToggleButtonFor(layerState:)`, & `layerStateFor(layerToggleButton:)` utility vars/methods.
    * New `selectLayerToggleButton(forLayerState:)` helper enabled the `isSelected` of the associated button, and disables `isSelected` for the rest of them.
    * Implemented `layerToggleButtonPressed(_:)` to set the `layerState` and update the UI via `selectLayerToggleButton(forLayerState:)`.
    * Also calling ``selectLayerToggleButton(forLayerState:)` in `viewDidLoad()` to for initial setup.

--- Sources/App/MapViewController.swift
+++ Sources/App/MapViewController.swift
@@ -12,6 +12,17 @@ import MapKit
 
 class MapViewController : UIViewController, MKMapViewDelegate
 {
+	enum LayerState {
+		case radar
+		case alerts
+		case stormReportsPoints
+		case stormReportsHeatmap
+		
+		static let `default`: Self = .radar
+	}
+	var layerState: LayerState = .default
+	
+	
 	@IBOutlet var mapView: MKMapView!
 	
 	@IBOutlet var radarButton: UIButton!
@@ -19,9 +30,48 @@ class MapViewController : UIViewController, MKMapViewDelegate
 	@IBOutlet var stormReportsPointsButton: UIButton!
 	@IBOutlet var stormReportsHeatmapButton: UIButton!
 	
+	lazy var allLayerToggleButtons: [UIButton] = [
+		self.radarButton,
+		self.alertsButton,
+		self.stormReportsPointsButton,
+		self.stormReportsHeatmapButton,
+	]
+	
+	func layerToggleButtonFor(layerState: LayerState) -> UIButton {
+		switch layerState {
+			case .radar:
+				return self.radarButton
+			case .alerts:
+				return self.alertsButton
+			case .stormReportsPoints:
+				return self.stormReportsPointsButton
+			case .stormReportsHeatmap:
+				return self.stormReportsHeatmapButton
+		}
+	}
+	
+	func layerStateFor(layerToggleButton: UIButton) -> LayerState? {
+		switch layerToggleButton {
+			case self.radarButton:
+				return .radar
+			case self.alertsButton:
+				return .alerts
+			case self.stormReportsPointsButton:
+				return .stormReportsPoints
+			case self.stormReportsHeatmapButton:
+				return .stormReportsHeatmap
+			
+			default: return nil
+		}
+	}
+	
+	
+	
 	
 	override func viewDidLoad()
 	{
+		selectLayerToggleButton(forLayerState: self.layerState)
+		
 		super.viewDidLoad()
 		// Do any additional setup after loading the view.
 	}
@@ -29,6 +79,23 @@ class MapViewController : UIViewController, MKMapViewDelegate
 	
 	@IBAction func layerToggleButtonPressed(_ senderButton: UIButton)
 	{
-		print("\(senderButton) pressed")
+		guard let layerState = layerStateFor(layerToggleButton: senderButton) else {
+			print("Warning: \(#function) ignoring received action from unhandled button \(senderButton).")
+			return
+		}
+		
+		self.layerState = layerState
+		selectLayerToggleButton(forLayerState: layerState)
+	}
+	
+	func selectLayerToggleButton(forLayerState: LayerState)
+	{
+		let selectedButton = layerToggleButtonFor(layerState: layerState)
+		let otherButtons = self.allLayerToggleButtons.filter{ $0 != selectedButton }
+		
+		// This could also be accomplished via `UIButton`'s `.changesSelectionAsPrimaryAction`, but we'd still need to un-select the other buttons.
+		// In general I prefer to have discrete state transitions all in one place, rather than two systems doing the same thing (can cause state desync). 
+		selectedButton.isSelected = true
+		otherButtons.forEach{ $0.isSelected = false }
 	}
 }
