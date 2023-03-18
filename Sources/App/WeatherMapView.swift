15a2ecb4438e26d457c1d13a9af7e06dd43061c9
Slipp Douglas Thompson <git@capnslipp.com>
2023-03-18 17:27:00 -0500

    Map overlay rendering
    Now (finally!) rendering overlays in `WeatherMapView`.
    
    * New `radarLayerOverlay` property sets up a `MKTileOverlay` to pull map tiles from the Aeris web point.  
    	‣ More can be done with this (it seems the caching is minimal), but this at least gets it running.
    * New `radarLayerOverlayRenderer` property sets up parallel `MKTileOverlayRenderer`.
    * New `setUpOverlaysForCurrentLayerState()` method called on init adds the overlay to the `MKMapView`.
    * Added `mapView(_: MKMapView, rendererFor overlay: MKOverlay)` hook to inform the `MKMapView` of what renderer to use.
    * Also, added an extension (like what was done in `MapViewController`) to associate `MKOverlay` ↔ `WeatherMapView.LayerState` with convenience methods/properties.  
    	‣ I'm not happy with this as a proper solution — it would be much cleaner to do something type-protocol-y or at the very least use a lookup dictionary — but it gets it up and working for now.

--- Sources/App/WeatherMapView.swift
+++ Sources/App/WeatherMapView.swift
@@ -20,10 +20,30 @@ class WeatherMapView : UIView, MKMapViewDelegate
 		
 		static let `default`: Self = .radar
 	}
-	public var layerState: LayerState = .default
+	public var layerState: LayerState = .default {
+		didSet {
+			guard self.layerState != oldValue else { return }
+			setUpOverlaysForCurrentLayerState()
+		}
+	}
 	
 	
 	var mapView: MKMapView!
+	
+	lazy var radarLayerOverlay: MKTileOverlay = {
+		let clientID = "mDDQDYPbqq4PK43usr9HJ"
+		let clientSecret = "nEyhFtwTSaCkBDKLpppDEOePPh1qw5qaeyuxYal6"
+		let overlay = MKTileOverlay(urlTemplate: "https://maps.aerisapi.com/\(clientID)_\(clientSecret)/radar/{z}/{x}/{y}/current.png")
+		overlay.canReplaceMapContent = false
+		return overlay
+	}()
+	lazy var radarLayerOverlayRenderer: MKTileOverlayRenderer = {
+		let renderer = MKTileOverlayRenderer(tileOverlay: self.radarLayerOverlay)
+		#if !targetEnvironment(macCatalyst)
+			renderer.blendMode = .multiply
+		#endif
+		return renderer
+	}()
 	
 	
 	public override init(frame: CGRect)
@@ -31,6 +51,7 @@ class WeatherMapView : UIView, MKMapViewDelegate
 		super.init(frame: frame)
 		
 		setUpSubviews()
+		setUpOverlaysForCurrentLayerState()
 	}
 	
 	/// Used by UIKit when unarchiving from a NIB/XIB/Storyboard.
@@ -39,6 +60,7 @@ class WeatherMapView : UIView, MKMapViewDelegate
 		super.init(coder: coder)
 		
 		setUpSubviews()
+		setUpOverlaysForCurrentLayerState()
 	}
 	
 	func setUpSubviews()
@@ -50,13 +72,65 @@ class WeatherMapView : UIView, MKMapViewDelegate
 		addSubview(mapView)
 		NSLayoutConstraint.activate(
 			[
-				"H:|-0-[mapView]-0-|",
-				"V:|-0-[mapView]-0-|"
+				"H:|-0-[view]-0-|",
+				"V:|-0-[view]-0-|"
 			].flatMap{
-				NSLayoutConstraint.constraints(withVisualFormat: $0, options: [ .directionLeftToRight ], metrics: nil, views: [ "mapView": mapView ])
+				NSLayoutConstraint.constraints(withVisualFormat: $0, options: [ .directionLeftToRight ], metrics: nil, views: [ "view": mapView ])
 			}
 		)
 		
 		self.mapView = mapView
+	}
+	
+	func setUpOverlaysForCurrentLayerState()
+	{
+		self.mapView.removeOverlays(self.allLayerOverlays)
+		
+		if let newOverlay = layerOverlay(forLayerState: self.layerState) {
+			// TODO: Remove `if let` once we have all states handled.
+			self.mapView.addOverlay(newOverlay, level: .aboveRoads)
+		}
+	}
+	
+	func mapView(_: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer
+	{
+		switch layerState(forLayerOverlay: overlay) {
+			case .radar:
+				return self.radarLayerOverlayRenderer
+			
+			default:
+				print("Warning: \(#function) ignoring unhandled overlay \(overlay).")
+				return MKPolygonRenderer(polygon: MKPolygon(points: [], count: 0))
+		}
+	}
+}
+
+
+
+/// `MKOverlay` ↔ `WeatherMapView.LayerState` Convenience Methods
+extension WeatherMapView
+{
+	var allLayerOverlays: [MKOverlay] {
+		[
+			self.radarLayerOverlay,
+		]
+	}
+	
+	func layerOverlay(forLayerState state: LayerState) -> MKOverlay? {
+		switch state {
+			case .radar:
+				return self.radarLayerOverlay
+			
+			default: return nil
+		}
+	}
+	
+	func layerState(forLayerOverlay overlay: MKOverlay) -> LayerState? {
+		switch overlay {
+			case let tileOverlay as MKTileOverlay where tileOverlay == self.radarLayerOverlay:
+				return .radar
+			
+			default: return nil
+		}
 	}
 }
